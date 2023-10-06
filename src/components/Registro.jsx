0aaebaf11f1db33c5d20624f589b9ef577d8c6b6
Maria jose cr <cotee.cr@gmail.com>
2023-10-06 00:06:31 -0300

    Se agrega componente SocialButton con sus respectivos iconos,usando props

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -7,7 +7,7 @@ function Registro({error,setError,setErrorMail,errorMail,setErrorPassword,errorP
     return (
     <>
         <h2 style={{textAlign: "center"}}>Crea una cuenta</h2>
-        <SocialButton />
+        <SocialButton primerIcono="fa-facebook" segundoIcono="fa-github"  tercerIcono="fa-instagram"/>
         <p> O usa tu email para registrarte </p>
         <Formulario setError={setError} setErrorMail={setErrorMail} setErrorPassword={setErrorPassword} />
     </>
