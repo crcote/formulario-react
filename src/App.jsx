22ac273e949b3793637063389d9a52787e08c825
Fel-Jim <jimenez.edu.cruz@gmail.com>
2023-10-06 22:49:53 -0300

    update with div className BodyBox with bootstrap class and other div ClasName PrincipalBox with bootstrap class in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import Registro from "./components/Registro"
-import { useState } from "react"
+import Registro from "./components/Registro";
+import { useState } from "react";
 function App() {
-  
   const [error, setError] = useState();
   const [errorMail, setErrorMail] = useState();
-  const [errorPassword, setErrorPassword] = useState (); //Los estados se inicializan en undefined para que se evite mostrar el menaje de registro exitoso antes de que el formulario sea valido
-  
+  const [errorPassword, setErrorPassword] = useState(); //Los estados se inicializan en undefined para que se evite mostrar el menaje de registro exitoso antes de que el formulario sea valido
+
   return (
     <>
-      <Registro 
-        setError={setError} 
-        error={error} 
-        setErrorMail={setErrorMail} 
-        errorMail={errorMail} 
-        setErrorPassword={setErrorPassword} 
-        errorPassword={errorPassword}
-      />      
+      <div className="BodyBox d-flex justify-content-center mt-5">
+        <div className="PrincipalBox d-grid border border-3 border-dark rounded-4 p-4">
+          <Registro
+            setError={setError}
+            error={error}
+            setErrorMail={setErrorMail}
+            errorMail={errorMail}
+            setErrorPassword={setErrorPassword}
+            errorPassword={errorPassword}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
