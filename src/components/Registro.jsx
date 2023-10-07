4985c449994b682c43e09c53884178f88666d6ae
Fel-Jim <jimenez.edu.cruz@gmail.com>
2023-10-06 22:42:42 -0300

    update registro.jsx return and function

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,17 +1,48 @@
-import Formulario from "./Formulario"
+import Formulario from "./Formulario";
 import SocialButton from "./SocialButton";
 import Alert from "./Alert";
 
-function Registro({error,setError,setErrorMail,errorMail,setErrorPassword,errorPassword}) {
-
-    return (
+function Registro({
+  error,
+  setError,
+  setErrorMail,
+  errorMail,
+  setErrorPassword,
+  errorPassword,
+}) {
+  return (
     <>
-        <h2 style={{textAlign: "center"}}>Crea una cuenta</h2>
-        <SocialButton primerIcono="fa-facebook" segundoIcono="fa-github"  tercerIcono="fa-instagram"/>
-        <p> O usa tu email para registrarte </p>
-        <Formulario setError={setError} setErrorMail={setErrorMail} setErrorPassword={setErrorPassword} />
+      <h2 class="text-white" style={{ textAlign: "center" }}>
+        Crea una cuenta
+      </h2>
+      <SocialButton
+        primerIcono="fa-facebook"
+        segundoIcono="fa-github"
+        tercerIcono="fa-instagram"
+      />
+      <p class="d-flex justify-content-center text-white">
+        {" "}
+        O usa tu email para registrarte{" "}
+      </p>
+      <Formulario
+        setError={setError}
+        setErrorMail={setErrorMail}
+        setErrorPassword={setErrorPassword}
+      />
+      {error ? (
+        <Alert message="Completa todos los campos" color="danger" />
+      ) : null}
+      {errorMail && !error ? (
+        <Alert message="Formato mail incorrecto" color="danger" />
+      ) : null}
+      {errorPassword && !error ? (
+        <Alert message="Contraseñas no coinciden" color="danger" />
+      ) : null}
+      {error == false && errorMail == false && errorPassword == false ? (
+        <Alert message="Registro Exitoso" color="success" />
+      ) : null}
     </>
-    )
+  );
 }
 
-export default Registro
+export default Registro;
